2b45fcc683f09de11dd89ab4887ca1117df6a0e0
Malykhin Andrej <Andrej.Malykhin@privatbank.ua>
2018-11-14 16:35:01 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+**https://dzone.com/articles/building-a-web-app-using-spring-boot-angular-6-and**
+
+**https://developer.okta.com/blog/2017/12/04/basic-crud-angular-and-spring-boot**
---
+++ server/src/main/java/com/giggs13/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.giggs13.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
